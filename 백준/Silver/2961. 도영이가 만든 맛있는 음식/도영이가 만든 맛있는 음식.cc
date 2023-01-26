6b95fb72da597f658494c1315d242701c75c5d23
이예은, Ye Eun Lee <76764598+synodical@users.noreply.github.com>
2023-01-26 13:48:55 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.cc
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.cc
@@ -5,7 +5,11 @@ using namespace std;
 using ll = long long;
 // 신맛은 곱이고 쓴 맛은 합이다.
 
+
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     int n, s, b;
     vector<pair<int, int>> v;
 
@@ -14,35 +18,21 @@ int main() {
         cin >> s >> b;
         v.push_back({s, b});
     }
-    sort(v.begin(), v.end());
-    int azi = 10000000;
-    for (int i = 0; i < n; ++i) {
-        vector<int> visit;
-        for (int j = 0; j < i; ++j) {
-            visit.push_back(0);
-        }
-        for (int j = n-i; j > 0; --j) {
-            visit.push_back(1);
-        }
-
-        do{
-            int sutpai = 1;
-            int nigai = 0;
-            for(int i=0; i<v.size(); i++) {
-
-                if (visit[i] != 0) {
-//                    cout << " " <<  v[i].first << " " << v[i].second << " ";
-                    sutpai *= v[i].first;
-                    nigai += v[i].second;
-                }
-
+    int agi = 10000000;
+    for (int i = 1; i < (1 << n); ++i) {
+        // 1 ~ 모든 조합 수인 2^(n)-1을 조사해야 함
+        // 1<<n == 2^n
+        int sutpai = 1, nigai = 0;
+        for (int j = 0; j < n; ++j) {
+            //
+            if (i & (1 << j)) {
+                sutpai *= v[j].first;
+                nigai += v[j].second;
             }
-            azi = min(azi, abs(sutpai - nigai));
-//            cout << '\n';
-        }while(next_permutation(visit.begin(),visit.end()));
-
+        }
+        agi = min(agi, abs(sutpai - nigai));
     }
-    cout << azi;
+    cout <<agi;
     return 0;
 }
 

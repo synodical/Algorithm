544186fc5a8957cd7940024901b3da16b4a6e077
이예은, Ye Eun Lee <76764598+synodical@users.noreply.github.com>
2023-02-07 12:36:47 +0900

    [Silver III] Title: N과 M (1), Time: 376 ms, Memory: 26588 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -11,9 +12,11 @@ public class Main {
     // numbers: 전체 데이터가 들어있는 배열
     static boolean[] isSelected;
     // 순열에 사용된 숫자인지 체크하기 위한 배열
+    static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
+
         N = sc.nextInt();
         R = sc.nextInt();
         numbers = new int[N];
@@ -23,17 +26,17 @@ public class Main {
             numbers[i] = i + 1;
         }
         perm(0);
-//        System.out.println("모든 경우의 수 : " + totalCount);
+        System.out.println(sb.toString());
     }
     // 현재까지 뽑힌 숫자 개수를 입력받아 체크한 뒤 종료하거나 숫자를 뽑음
     // cnt = 이제까지 뽑은 순열을 구성하는 숫자 개수
-    private static void perm(int cnt) {
+    private static void perm(int cnt) throws IOException {
         if (cnt == R) { // 다 뽑음
             totalCount++;
             for (int i = 0; i < result.length; i++) {
-                System.out.print(result[i] + " ");
+                sb.append(result[i]).append(" ");
             }
-            System.out.println();
+            sb.append("\n");
             return;
         }
         for (int i = 0; i < N; i++) { // 전체 숫자를 대상으로 뽑기 (0~N)
@@ -44,5 +47,6 @@ public class Main {
             perm(cnt + 1); // 다음 숫자 뽑으러 보냄 (다음 사람한테 보냄)
             isSelected[i] = false; // 사용했던 위치 해제
         }
+
     }
 }

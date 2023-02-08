b647a2d7a571a3e662faf6efce0df840410a2700
이예은, Ye Eun Lee <76764598+synodical@users.noreply.github.com>
2023-02-08 09:30:39 +0900

    [Gold IV] Title: 트리의 지름, Time: 228 ms, Memory: 22436 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -37,39 +37,38 @@ public class Main {
             ans = cur_w;
             far_node = cur;
         }
+        // 현재 보고 있는 cur node에 연결된 노드를 dfs 탐색
+        // cur 에 연결된 노드의 번호와 누적된 가중치 재귀함수 전달
         for (Pair p : tree[cur]) {
-            int nxt = p.getC();
-            sol(nxt, cur_w + p.getW());
+            sol(p.getC(), cur_w + p.getW());
         }
     }
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         if (n == 1) {
-            System.out.println("0");
+            System.out.println(0);
             return;
         }
         tree = new ArrayList[n + 1];
         visited = new boolean[n + 1];
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i <= n; i++) {
             tree[i] = new ArrayList<Pair>();
         }
-
         for (int i = 0; i < n - 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int p = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
             tree[p].add(new Pair(c, w));
-            tree[c].add(new Pair(p, w));// 양방향 그래프
+            tree[c].add(new Pair(p, w)); // 양방향 그래프
 
         }
-        sol(1,0);
-
-        for (int i = 0; i < visited.length; i++) visited[i] = false;
-
+        sol(1, 0);
+        for (int i = 0; i <= n; i++) {
+            visited[i] = false;
+        }
         sol(far_node, 0);
         System.out.println(ans);
-
     }
 }

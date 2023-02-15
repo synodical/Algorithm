a994ec5f80a24705f45d0ebc8dbe15bac7965d22
이예은, Ye Eun Lee <76764598+synodical@users.noreply.github.com>
2023-02-15 17:42:53 +0900

    [Silver V] Title: 색종이, Time: 128 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,36 @@
+import javax.naming.spi.DirObjectFactory;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int t;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        t = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[100][100];
+        int ans = 0;
+        while (t-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            for (int i = a; i < a + 10; i++) {
+                for (int j = b; j < b + 10; j++) {
+                    arr[i][j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (arr[i][j] == 1) {
+                    ans++;
+                }
+            }
+        }
+        System.out.println(ans);
+
+    }
+
+}

5f4bac10f9a90ba64b2b3baf189279471c0c8368
이예은, Ye Eun Lee <76764598+synodical@users.noreply.github.com>
2023-02-15 17:44:59 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -18,19 +18,14 @@ public class Main {
             int b = Integer.parseInt(st.nextToken());
             for (int i = a; i < a + 10; i++) {
                 for (int j = b; j < b + 10; j++) {
-                    arr[i][j] = 1;
-                }
-            }
-        }
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                if (arr[i][j] == 1) {
-                    ans++;
+                    if (arr[i][j] != 1) {
+                        ans++;
+                        arr[i][j] = 1;
+                    }
                 }
             }
         }
         System.out.println(ans);
-
     }
 
 }
